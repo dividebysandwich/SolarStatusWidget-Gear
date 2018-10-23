e9902a520043f2b24c02acdb2537292b058261de
Toumal <toumaltheorca@gmail.com>
2018-10-23 19:41:07 +0200

    Fixed background/foreground switching, added energy flow indicators

--- widget/SolarWidget/js/app.js
+++ widget/SolarWidget/js/app.js
@@ -28,7 +28,6 @@
 	TFT_DARKGREY = "#222222",
 	lengthNews = 0;
 
-	var myCanvasContext;
 	
 	/**
 	 * Removes all child of the element.
@@ -86,235 +85,53 @@
 		} else {
 			trimmedString = text;
 		}
-
 		return trimmedString;
 	}
 
-	function drawLine(startx, starty, endx, endy, color) {
-		myCanvasContext.beginPath();
-		myCanvasContext.strokeStyle=color;
-		myCanvasContext.lineWidth=1;
-		myCanvasContext.moveTo(startx, starty);
-		myCanvasContext.lineTo(endx, endy);
-		myCanvasContext.stroke();
-	}	
-
-	var animationOffset = 0;
-	var maxAnimationOffset = 140;
-	function drawHorizontalPowerAnimation(startx, endx, starty, endy, num_dots, dir)
-	{
-		if (dir === 0) {
-			var x = 0;
-			while (x < (endx - startx)) {
-				if (x === animationOffset || (num_dots > 1 && x === animationOffset - 15) || (num_dots > 2 && x === animationOffset - 30) || (num_dots > 3 && x === animationOffset - 45) ) {
-					drawLine(x+startx, starty, x+startx, endy, TFT_WHITE);
-					x++;
-					if (x < (endx - startx)) {
-						drawLine(x+startx, starty, x+startx, endy, TFT_WHITE);
-						x++;
-					}
-					if (x < (endx - startx)) {
-						drawLine(x+startx, starty, x+startx, endy, TFT_WHITE);
-						x++;
-					}
-				} else {
-					drawLine(x+startx, starty, x+startx, endy, TFT_DARKGREY);
-					x++;
-				}
-			}
-		} else {
-			var x = (endx - startx);
-			while (x > 0) {
-				if (x === (maxAnimationOffset-animationOffset) || (num_dots > 1 && x === (maxAnimationOffset-animationOffset) - 15) || (num_dots > 2 && x === (maxAnimationOffset-animationOffset) - 30) || (num_dots > 3 && x === (maxAnimationOffset-animationOffset) - 45)) {
-					drawLine(x+startx, starty, x+startx, endy, TFT_WHITE);
-					x--;
-					if (x > 0) {
-						drawLine(x+startx, starty, x+startx, endy, TFT_WHITE);
-						x--;
-					}
-					if (x > 0) {
-						drawLine(x+startx, starty, x+startx, endy, TFT_WHITE);
-						x--;
-					}
-				} else {
-					drawLine(x+startx, starty, x+startx, endy, TFT_DARKGREY);
-					x--;
-				}
-			}
-		}
-	}
-
-	function drawVerticalPowerAnimation(startx, endx, starty, endy, num_dots, dir)
-	{
-		if (dir === 0) {
-			var y = 0;
-			while (y < (endy - starty)) {
-				if (y === animationOffset || (num_dots > 1 && y === animationOffset - 15) || (num_dots > 2 && y === animationOffset - 30) || (num_dots > 3 && y === animationOffset - 45) ) {
-					drawLine(startx, y+starty, endx, y+starty, TFT_WHITE);
-					y++;
-					if (y < (endy - starty)) {
-						drawLine(startx, y+starty, endx, y+starty, TFT_WHITE);
-						y++;
-					}
-					if (y < (endy - starty)) {
-						drawLine(startx, y+starty, endx, y+starty, TFT_WHITE);
-						y++;
-					}
-				} else {
-					drawLine(startx, y+starty, endx, y+starty, TFT_DARKGREY);
-					y++;
-				}
-			}
-		} else {
-			var y = (endy - starty);
-			while (y > 0) {
-				if (y === (maxAnimationOffset-animationOffset) || (num_dots > 1 && y === (maxAnimationOffset-animationOffset) - 15) || (num_dots > 2 && y === (maxAnimationOffset-animationOffset) - 30) || (num_dots > 3 && y === (maxAnimationOffset-animationOffset) - 45)) {
-					drawLine(startx, y+starty, endx, y+starty, TFT_WHITE);
-					y--;
-					if (y > 0) {
-						drawLine(startx, y+starty, endx, y+starty, TFT_WHITE);
-						y--;
-					}
-					if (y > 0) {
-						drawLine(startx, y+starty, endx, y+starty, TFT_WHITE);
-						y--;
-					}
-				} else {
-					drawLine(startx, y+starty, endx, y+starty, TFT_DARKGREY);
-					y--;
-				}
-			}
-		}
-	}
-
-
-	function advanceAnimation()
+	
+	
+	
+	function drawPowerLinks()
 	{
 		if (arrayData.length < 4) {
-			window.requestAnimationFrame(advanceAnimation);
 			return;
 		}
 		//Battery usage animation
-		var numBattArrows = 0;
-		var battDirection = 0;
 		var fbattuse = parseFloat(arrayData[4]);
-		if (fbattuse < -0.1) {
-			if (fbattuse >= -0.7) {
-				numBattArrows = 1;
-			} else if (fbattuse >= -1.4) {
-				numBattArrows = 2;
-			} else {
-				numBattArrows = 3;
-			}
-			battDirection = 0;
-			drawHorizontalPowerAnimation(70, 156, 175, 183, numBattArrows, battDirection);
-		} else if (fbattuse > 0.1) {
-			if (fbattuse <= 0.7) {
-				numBattArrows = 1;
-			} else if (fbattuse <= 1.4) {
-				numBattArrows = 2;
-			} else {
-				numBattArrows = 3;
-			}
-			battDirection = 1;
-			drawHorizontalPowerAnimation(70, 156, 175, 183, numBattArrows, battDirection);
+		var linkbatt = document.getElementById("linkbatt");
+		if (fbattuse < -0.1 || fbattuse > 0.1) {
+			linkbatt.style.display = "block";
+		} else {
+			linkbatt.style.display = "none";
 		}
+			
 
 		//Grid usage animation
-		var numGridArrows = 0;
-		var gridDirection = 0;
 		var fgrid = parseFloat(arrayData[3]);
-		if (fgrid < -0.1) {
-			if (fgrid >= -1.0) {
-				numGridArrows = 1;
-			} else if (fgrid >= -2.0) {
-				numGridArrows = 2;
-			} else if (fgrid >= -6.0) {
-				numGridArrows = 3;
-			} else {
-				numGridArrows = 4;
-			}
-			gridDirection = 0;
-			drawHorizontalPowerAnimation(210, 305, 175, 183, numGridArrows, gridDirection);
-		} else if (fgrid > 0.1) {
-			if (fgrid <= 1.0) {
-				numGridArrows = 1;
-			} else if (fgrid <= 2.0) {
-				numGridArrows = 2;
-			} else if (fgrid <= 6.0) {
-				numGridArrows = 3;
-			} else {
-				numGridArrows = 4;
-			}
-			gridDirection = 1;
-			drawHorizontalPowerAnimation(210, 305, 175, 183, numGridArrows, gridDirection);
+		var linkgrid = document.getElementById("linkgrid");
+		if (fgrid < -0.1 || fgrid > 0.1) {
+			linkgrid.style.display = "block";
+		} else {
+			linkgrid.style.display = "none";
 		}
 
 		//PV usage animation
-		var numPVArrows = 0;
-		var pvDirection = 0;
 		var fpv = parseFloat(arrayData[1]);
-		if (fpv < -0.1) {
-			if (fpv >= -1.0) {
-				numPVArrows = 1;
-			} else if (fpv >= -2.0) {
-				numPVArrows = 2;
-			} else if (fpv >= -6.0) {
-				numPVArrows = 3;
-			} else {
-				numPVArrows = 4;
-			}
-			pvDirection = 1;
-			drawVerticalPowerAnimation(183, 187, 53, 153, numPVArrows, pvDirection);
-		} else if (fpv > 0.1) {
-			if (fpv <= 1.0) {
-				numPVArrows = 1;
-			} else if (fpv <= 2.0) {
-				numPVArrows = 2;
-			} else if (fpv <= 6.0) {
-				numPVArrows = 3;
-			} else {
-				numPVArrows = 4;
-			}
-			pvDirection = 0;
-			drawVerticalPowerAnimation(179, 187, 53, 153, numPVArrows, pvDirection);
+		var linkgrid = document.getElementById("linkgrid");
+		if (fpv < -0.1 || fpv > 0.1) {
+			linkpv.style.display = "block";
+		} else {
+			linkpv.style.display = "none";
 		}
 
 		//house consumer usage animation
-		var numUseArrows = 0;
-		var useDirection = 0;
 		var fuse = parseFloat(arrayData[2]);
-		if (fuse < -0.1) {
-			if (fuse >= -1.0) {
-				numUseArrows = 1;
-			} else if (fuse >= -2.0) {
-				numUseArrows = 2;
-			} else if (fuse >= -6.0) {
-				numUseArrows = 3;
-			} else {
-				numUseArrows = 4;
-			}
-			useDirection = 1;
-			drawVerticalPowerAnimation(179, 187, 207, 295, numUseArrows, useDirection);
-		} else if (fuse > 0.1) {
-			if (fuse <= 1.0) {
-				numUseArrows = 1;
-			} else if (fuse <= 2.0) {
-				numUseArrows = 2;
-			} else if (fuse <= 6.0) {
-				numUseArrows = 3;
-			} else {
-				numUseArrows = 4;
-			}
-			useDirection = 0;
-			drawVerticalPowerAnimation(179, 187, 207, 295, numUseArrows, useDirection);
+		var linkuse = document.getElementById("linkuse");
+		if (fuse < -0.1 || fuse > 0.1) {
+			linkuse.style.display = "block";
+		} else {
+			linkuse.style.display = "none";
 		}
-
-		animationOffset++;
-		if (animationOffset > maxAnimationOffset) {
-			animationOffset = 0;
-		}
-		window.requestAnimationFrame(advanceAnimation);
 
 	}
 
@@ -342,11 +159,6 @@
 		var battfill  = document.getElementById("battfill");
 		battfill.style.height = Math.round((parseFloat(arrayData[0]) / 100.0) * 70.0) + "px";
 
-		var c = document.getElementById("myCanvas");
-		c.setAttribute('width', '360');
-		c.setAttribute('height', '360');
-		myCanvasContext = c.getContext("2d");
-		myCanvasContext.clearRect(0,0,359,359);
 
 	}
 
@@ -376,6 +188,7 @@
 				if (result.length >4) {
 					arrayData = result;
 					showNews();
+					drawPowerLinks();
 				}
 				xmlhttp = null;
 			} else {
@@ -388,7 +201,6 @@
 
 
 	var dataFetchTimer = false;
-	var animationTimer = false;
 	
 	/**
 	 * Initiates the application.
@@ -396,10 +208,8 @@
 	 */
 	function init() {
 		getDataFromXML();
-		dataFetchTimer = setInterval(getDataFromXML, 20000);
-//		animationTimer = setInterval(advanceAnimation, 20);
-		document.addEventListener(visibilityChange, handleVisibilityChange);
-		window.requestAnimationFrame(advanceAnimation);
+		dataFetchTimer = setInterval(getDataFromXML, 60000);
+		document.addEventListener('visibilitychange', handleVisibilityChange);
 	}
 
 	function handleVisibilityChange(){
@@ -409,18 +219,11 @@
 				clearInterval(dataFetchTimer);
 				dataFetchTimer = false;
 			}
-			if (animationTimer !== false) {
-				clearInterval(animationTimer);
-				animationTimer = false;
-			}
 		} else {
 			console.log("Page is now visible.");
 			getDataFromXML();
 			if (dataFetchTimer === false)
-				dataFetchTimer = setInterval(getDataFromXML, 20000);
-//			if (animationTimer === false)
-//			    animationTimer = setInterval(advanceAnimation, 20);
-			window.requestAnimationFrame(advanceAnimation);
+				dataFetchTimer = setInterval(getDataFromXML, 60000);
 		}
 	}
 	
